205874313433a5bc3ccb81cc73b6bd32af0623e5
AgentOxygen <cameronscummins@gmail.com>
2022-01-09 14:23:47 -0600

    Created tic tac toe libGDX project

---
+++ tic_tac_toe_proj/desktop/src/com/agentoxygen/ttt/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.agentoxygen.ttt.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.agentoxygen.ttt.TicTacToe;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TicTacToe(), config);
+	}
+}
