3b8e3cd3bc1ff3f365964063fa9bdaff028bd09d
AgentOxygen <cameronscummins@gmail.com>
2022-01-24 23:24:47 -0600

    Created speedrunner project

---
+++ speedrunner/desktop/src/com/agentoxygen/speed/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.agentoxygen.speed.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.agentoxygen.speed.SpeedRunner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpeedRunner(), config);
+	}
+}
