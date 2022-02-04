c85c5009ce3511a3822d4b3463e1854c35539dbf
AgentOxygen <cameronscummins@gmail.com>
2022-02-04 15:11:34 -0600

    Last update before starting on main game

---
+++ speedrunner/core/assets/Scifi_kit.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Scifi_kit" tilewidth="32" tileheight="32" tilecount="805" columns="35">
+ <image source="scifi_platformTiles_32x32.png" width="1120" height="736"/>
+</tileset>
